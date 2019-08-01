31f6862a6e90d3cab76abd5efc2c9b0c835d86c4
Erik Öhrn <erik.ohrn@outlook.com>
2019-08-01 21:48:56 +0200

    More info

--- demo/docs/Forms/FormsPage.vue
+++ demo/docs/Forms/FormsPage.vue
@@ -194,6 +194,18 @@
         </mdb-col>
       </mdb-row>
     </mdb-container>
+    <form>
+    <p class="h4 text-center mb-4">Write to us</p>
+    <div class="grey-text">
+      <mdb-input label="Your name" icon="user" group type="text" validate error="wrong" success="right"/>
+      <mdb-input label="Your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
+      <mdb-input label="Subject" icon="tag" group type="text" validate error="wrong" success="right"/>
+      <mdb-textarea :rows="2" label="Your message" icon="pencil"/>
+    </div>
+    <div class="text-center">
+      <mdb-btn outline="secondary">Send <mdb-icon far icon="paper-plane" class="ml-1"/></mdb-btn>
+    </div>
+  </form>
   </mdb-container>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="hello">
     <h3>{{ msg }}</h3>
-    <p class="mb-2">The application is configured and ready to import our components. You will find MDB Vue docs <a href="https://mdbootstrap.com/vue/" target="_blank">here</a>.</p>
-    <p>Please type <span class="black-text">npm run demo</span> or <span class="black-text">yarn demo</span> to launch our Demonstration Page. The files with the code are placed in the <span class="black-text">demo</span> directory.</p>
-    <p>If You want to completely remove our demo from Your App, easily type <span class="black-text">npm run remove-demo</span> or <span class="black-text">yarn remove-demo</span>.</p>
+    <p class="mb-2">Hallå eller! Den här sidan är väldigt mycket under konstruktion.</p>
+    <p>Man kan kolla dokumentationen för MDB <a href="https://mdbootstrap.com/vue/" target="_blank">här</a> (för Eriks del).</p>
+    <p>Inputfältet nedan går till en temporär mail. Testa att skicka ett meddelande om du vill!</p>
  
  
- <form action="//formspree.io/zonadexe@rmailgroup.in" method="POST">
- <fieldset>
- <label for="name">Your name</label><br>
- <input type="text" name="name" placeholder="Name" id="name" required>
- </fieldset>
- <fieldset>
- <label for="_replyto">Your email</label><br>
- <input type="email" name="_replyto" placeholder="example@domain.com" id="_replyto" required>
- </fieldset>
- <fieldset>
- <label for="message">Your message</label><br>
- <textarea name="message" rows="1" placeholder="Message" id="message" required></textarea>
- </fieldset>
- <input class="hidden" type="text" name="_gotcha" style="display:none">
- <input class="hidden" type="hidden" name="_subject" value="Message via http://domain.com">
-<input class="button submit" type="submit" value="Send">
-</form>
+  <form :action="`//formspree.io/${mail}`" method="POST">
+  <fieldset>
+  <label for="name">Your name</label><br>
+  <input type="text" name="name" placeholder="Name" id="name" required>
+  </fieldset>
+  <fieldset>
+  <label for="_replyto">Your email</label><br>
+  <input type="email" name="_replyto" placeholder="example@domain.com" id="_replyto" required>
+  </fieldset>
+  <fieldset>
+  <label for="message">Your message</label><br>
+  <textarea name="message" rows="1" placeholder="Message" id="message" required></textarea>
+  </fieldset>
+  <input class="hidden" type="text" name="_gotcha" style="display:none">
+  <input class="hidden" type="hidden" name="_subject" value="Message via http://domain.com">
+  <input class="button submit" type="submit" value="Send">
+  </form>
   </div>
 </template>
 
@@ -31,7 +31,8 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your MDB Vue.js App'
+      msg: 'Välkommen till Pelles sida',
+      mail: `${'hutrojumlo'}${'@'}desoz.com`,
     };
   }
 }
@@ -47,6 +48,6 @@ h3 {
 p {
   color: #969696;
   margin-bottom: 0;
-  font-size: 14px;
+  font-size: 1.25rem;
 }
 </style>
